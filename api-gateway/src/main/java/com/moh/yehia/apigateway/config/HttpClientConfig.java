c4579502bfb8e7033bfea5007adb646df58eb7b5
mohyehia <mohammedyehia99@gmail.com>
2023-12-21 14:46:05 +0200

    Add routes to the api-gateway manually and disable the automatic route discovery from api-gateway

---
+++ api-gateway/src/main/java/com/moh/yehia/apigateway/config/HttpClientConfig.java
@@ -0,0 +1,15 @@
+package com.moh.yehia.apigateway.config;
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.netty.http.client.HttpClient;
+
+@Configuration
+public class HttpClientConfig {
+    @Bean
+
+    public HttpClient httpClient() {
+        return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+    }
+}
