3c9b0baf2ff8f65f01bd9afeb0054d4c73f7888a
mohyehia <mohammedyehia99@gmail.com>
2024-08-22 19:58:36 +0300

    Adding `spring-docs` for generating openapi documentation

---
+++ licence-service/src/main/java/com/moh/yehia/licence/service/config/OpenAPIConfig.java
@@ -0,0 +1,23 @@
+package com.moh.yehia.licence.service.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Licence Service API")
+                        .description("Licence Service API Documentation")
+                        .version("1.0.0")
+                        .license(new License().name("Apache 2.0").url("https://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("You can refer to this Wiki for more documentation")
+                        .url("/api-docs"));
+    }
+}
